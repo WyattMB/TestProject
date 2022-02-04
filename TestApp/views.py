7cb0abe96b9274734693e293e40e53bea1180adc
WyattMB <wyattbas@yahoo.com>
2022-02-04 16:21:53 -0500

    Initial commit

---
+++ TestApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def testview(request):
+    return HttpResponse("This is a test view and url for TestProject")
